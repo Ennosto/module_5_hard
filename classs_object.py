69b958f77aaa0f7e031a8632d00e82439befcd3c
Ennosto <ennosto_me@mail.ru>
2024-09-16 16:47:24 +0300

    Update classs_object.py
    Проверка на значение adult_mode

--- classs_object.py
+++ classs_object.py
@@ -37,17 +37,18 @@ class UrTube:
         return list_movie
 
     def watch_video(self, movie):
-        if self.current_user and self.current_user.age < 18:
-            print('Вам нет 18 лет, пожалуйста покиньте страницу')
-        elif self.current_user:
+        if self.current_user:
             for video in self.videos:
-                if movie in video.title:
-                    for i in range(1, 11):
-                        print(i, end=' ')
-                        time.sleep(1)
-                    print('Конец видео')
-        else:
-            print('Войдите в аккаунт, чтобы смотреть видео')
+                if movie == video.title:
+                    if video.adult_mode is True:
+                        if self.current_user.age < 18:
+                            print('Вам нет 18 лет, пожалуйста покиньте страницу')
+                        elif movie in video.title:
+                            for i in range(1, 11):
+                                print(i, end=' ')
+                                time.sleep(1)
+                            print('Конец видео')
+
 
     def __str__(self):
         return f"{self.videos}"
